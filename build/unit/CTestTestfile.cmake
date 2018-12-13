aacfefa0833c29ba5c55cb8bf7e38aaad4147669
Michael Wilson <mewil@umich.edu>
2018-12-13 18:32:59 -0500

    change to csv

--- build/unit/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/eligoldweber/Desktop/Michigan/School/1Fall2018/DistSystems/Research/libpaxos/unit
-# Build directory: /Users/eligoldweber/Desktop/Michigan/School/1Fall2018/DistSystems/Research/libpaxos/build/unit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit-tests "runtest")
-subdirs("gtest")
